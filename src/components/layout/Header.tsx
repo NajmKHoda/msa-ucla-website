09e6420bf96d2212b4e98de1a7de5b73b35c3c03
Najm Hoda <najmkhoda@gmail.com>
2025-02-28 11:22:55 -0800

    Use framer-motion for header floating

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,24 +1,23 @@
 'use client';
 
+import { useMotionValueEvent, useScroll } from 'framer-motion';
 import Image from 'next/image';
-import { useLayoutEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function Header() {
-    const [scrollPosition, setScrollPosition] = useState(0);
-   
+    const { scrollYProgress } = useScroll();
+    const [isFloating, setIsFloating] = useState(false);
+    
+    useMotionValueEvent(scrollYProgress, 'change', scrollYValue => {
+        if (scrollYValue > 0 && !isFloating) {
+            setIsFloating(true);
+        } else if (scrollYValue === 0 && isFloating) {
+            setIsFloating(false);
+        }
+    })
+
     const navLinkStyle = 'relative transition-[color] duration-200 ease-out after:bg-msa-yellow after:absolute after:inset-x-0 after:bottom-[-2px] after:h-[3px] after:scale-x-0 after:origin-left after:transition-all after:duration-200 after:ease-out hover:text-msa-yellow hover:after:scale-x-100';
-
-    useLayoutEffect(() => {
-        // Set initial scroll position on the client side
-        setScrollPosition(window.scrollY);
-
-        // Update scroll position on scroll
-        const onScroll = () => { setScrollPosition(window.scrollY); }
-        window.addEventListener('scroll', onScroll);
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
-
-    const floatClass = scrollPosition > 0 ? 'bg-msa-blue shadow-md' : '';
+    const floatClass = isFloating ? 'bg-msa-blue shadow-md' : '';
 
     return (
         <header className={`fixed z-50 flex justify-between items-center w-full transition duration-200 px-6 py-3 ${floatClass}`}>
