e047534bf664d68fe33d4ed3d9f4893c52b6e1f0
Najm Hoda <najmkhoda@gmail.com>
2025-03-26 18:38:25 -0700

    Reorganize header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -5,18 +5,10 @@ import projects from '@/../data/projects.json';
 import committees from '@/../data/committees.json';
 
 export default function Header() {
-
-
     return (
         <ResponsiveHeader>
             <MajorNav title='About'>
-                <MinorNav title='Mission Statement' />
                 <MinorNav title='Constitution' />
-            </MajorNav>
-            <MajorNav title='Events'>
-                <MinorNav title='Calendar' />
-                <MinorNav title='Upcoming' />
-                <MinorNav title='Past' />
             </MajorNav>
             <MajorNav title='Initiatives'>
                 <MinorNav title='Projects'>
@@ -29,7 +21,6 @@ export default function Header() {
                         <MinorNav key={id} title={name} href={`/committees#${id}`} />
                     ))}
                 </MinorNav>
-                <MinorNav title='Organizations' />
             </MajorNav>
             <MajorNav title='Resources'>
                 <MinorNav title='New Students' />
@@ -37,6 +28,7 @@ export default function Header() {
                 <MinorNav title='Chaplaincy' />
                 <MinorNav title='Report an Incident' />
             </MajorNav>
+            <MajorNav title='Events' />
             <MajorNav title='Donate' />
             <MajorNav title='Contact' />
         </ResponsiveHeader>
