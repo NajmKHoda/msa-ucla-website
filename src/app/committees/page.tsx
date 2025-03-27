1aac4657336530c6a5c697a54043f5ecfdcf63a4
Najm Hoda <najmkhoda@gmail.com>
2025-03-27 14:54:37 -0700

    Use grid structure for committees page

--- src/app/committees/page.tsx
+++ src/app/committees/page.tsx
@@ -4,13 +4,15 @@ export default function CommitteesPage() {
     return (
         <div className='flex flex-col gap-8 p-10'> 
             <h1 className='text-center text-5xl font-bold'>Committees</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
             {Object.entries(committeesData).map(([id, { name, description, heads }]) =>
-                <section key={id} className='flex flex-col items-center text-center sm:items-start sm:text-start gap-2'>
-                    <h2 id={id} className="text-4xl font-semibold">{name}</h2>
+                <section key={id} className='flex flex-col items-center justify-center p-6 text-center lg:items-start lg:text-start gap-2 bg-bg-secondary rounded-2xl'>
+                    <h2 id={id} className="text-4xl font-semibold text-msa-blue">{name}</h2>
                     <p><strong>Committee {heads.length > 1 ? 'Heads' : 'Head'}:</strong> {heads.join(', ')}</p>
                     <p>{description}</p>
                 </section>
             )}
+            </div>
         </div>
     );
 }
