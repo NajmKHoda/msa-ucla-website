e8beaec7d40c8605ca5737b89072c64dcb4d8ff4
Najm Hoda <najmkhoda@gmail.com>
2025-04-05 23:02:21 -0700

    Add links to Chaplaincy page

--- src/app/resources/chaplaincy/page.tsx
+++ src/app/resources/chaplaincy/page.tsx
@@ -24,7 +24,7 @@ export default function ChaplaincyPage() {
                     <Link href='https://docs.google.com/forms/d/e/1FAIpQLSfmq0PUE2Ci-otLePW_yZGrSo2LfpnVIdVnLXvnKF2m6I0CPQ/viewform' className='block p-3 bg-msa-blue text-text-secondary rounded-lg'>Question Box</Link>
                 </div>
                 <p>
-                    Tabari Zahir is the Muslim chaplain at UCLA through the IOK CHESS chaplaincy program.
+                    Tabari Zahir is the Muslim chaplain at UCLA through <Link href='https://iokchess.com/chaplaincy/' className='text-msa-blue'>the IOK CHESS chaplaincy program</Link>.
                     A dedicated student and teacher of Islamic knowledge, he&apos;s known for integrating spirituality with social justice. 
                     He provides spiritual guidance, counseling, and mentorship to students on campus.
                 </p>
@@ -41,7 +41,7 @@ export default function ChaplaincyPage() {
                     <div>
                         <h2 className='text-2xl my-2 text-msa-blue'>Other Initiatives</h2>
                         <ul className='list-disc list-inside'>
-                            <li>Lamps of Light Project</li>
+                            <li><Link href='https://lampsoflight.org/' className='text-msa-blue'>Lamps of Light Project</Link></li>
                             <li>Khutbahs across Southern California</li>
                             <li>LA&apos;s first Muslim inpatient recovery center</li>
                         </ul>
